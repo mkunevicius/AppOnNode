d20c059bfe57cd1e756053a2d2314b857084e1d8
Mindaugas <mkunevicius@gmail.com>
2016-05-03 15:18:46 +0300

    local server removed

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var router = require("./router.js");
 //Solution: Use Node.js to perform the profile look ups and server our template via HTTP
 
 //Create a web server
-var http = require('http');
-http.createServer(function (request, response) {
-  router.home(request, response);
-  router.user(request, response);
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+// var http = require('http');
+// http.createServer(function (request, response) {
+//   router.home(request, response);
+//   router.user(request, response);
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337/');
